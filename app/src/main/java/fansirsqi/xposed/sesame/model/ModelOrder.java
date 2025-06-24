e3ca120f55b33eed7bf1e457e913236fbfc076ba
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-06-24 19:44:09 +0800

    精简功能（隐藏一下功能）
    如果你配置文件已经开启了隐藏的功能，那很抱歉，他还会运行~

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.java
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.java
@@ -25,17 +25,17 @@ public class ModelOrder {
             AntFarm.class,//庄园
             AntOrchard.class,//农场
             AntOcean.class,//海洋
-            AntDodo.class,//神奇物种
-            AncientTree.class,//古树
-            AntCooperate.class,//合种
-            Reserve.class,//保护地
+//          AntDodo.class,//神奇物种
+//          AncientTree.class,//古树
+//          AntCooperate.class,//合种
+//          Reserve.class,//保护地
             AntSports.class,//运动
-            AntMember.class,//会员
-            AntStall.class,//蚂蚁新村
-            GreenFinance.class,//绿色经营
-//            AntBookRead.class,//读书
-//            ConsumeGold.class,//消费金
-//            OmegakoiTown.class,//小镇
+//          AntMember.class,//会员
+//          AntStall.class,//蚂蚁新村
+//          GreenFinance.class,//绿色经营
+//          AntBookRead.class,//读书
+//          ConsumeGold.class,//消费金
+//          OmegakoiTown.class,//小镇
             AnswerAI.class,//AI答题
     };
     @Getter private  static final List<Class<? extends Model>> clazzList = new ArrayList<>();
