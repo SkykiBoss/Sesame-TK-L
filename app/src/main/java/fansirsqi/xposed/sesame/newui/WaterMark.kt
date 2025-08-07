e3ac28bdb25ec9826f23083f8a2de75815e0620c
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-08-07 15:20:39 +0800

    fix(ui): 修正水印UID字段引用

--- app/src/main/java/fansirsqi/xposed/sesame/newui/WaterMark.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/newui/WaterMark.kt
@@ -6,7 +6,7 @@ import android.graphics.Paint
 import android.view.ViewGroup
 import androidx.core.graphics.toColorInt
 import androidx.core.graphics.withRotation
-import fansirsqi.xposed.sesame.data.ViewAppInfo.androidId
+import fansirsqi.xposed.sesame.data.ViewAppInfo.verifyId
 
 class WatermarkView(context: android.content.Context) : android.view.View(context) {
 
@@ -22,7 +22,7 @@ class WatermarkView(context: android.content.Context) : android.view.View(contex
 
     var watermarkText: String? = null
         set(value) {
-            val prefixLines = listOf("免费模块仅供学习", "勿在国内平台传播,倒卖必死全家!", "UID: $androidId")
+            val prefixLines = listOf("免费模块仅供学习", "勿在国内平台传播,倒卖必死全家!", "UID: $verifyId", "ALLG打包版本")
             val combinedLines = if (value.isNullOrBlank()) {
                 prefixLines
             } else {
@@ -47,7 +47,7 @@ class WatermarkView(context: android.content.Context) : android.view.View(contex
 
     fun setInfo(tags: List<String>) {
         watermarkText = buildString {
-            appendLine("免费模块仅供学习\n勿在国内平台传播,倒卖必死全家!!")
+            appendLine("免费模块仅供学习\n勿在国内平台传播,倒卖必死全家!!\nALLG打包版本")
             appendLine(tags.joinToString(" | "))
         }
     }
