8ab954f2cea3971aa95b986c870628d40d11cbb8
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-08-19 17:11:42 +0800

    026+

--- app/src/main/java/fansirsqi/xposed/sesame/hook/ApplicationHook.java
+++ app/src/main/java/fansirsqi/xposed/sesame/hook/ApplicationHook.java
@@ -30,13 +30,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.lang.reflect.Method;
-import java.lang.reflect.Member;
-import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.atomic.AtomicReference;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import fansirsqi.xposed.sesame.BuildConfig;
@@ -126,27 +123,7 @@ public class ApplicationHook implements IXposedHookLoadPackage {
         dayCalendar.set(Calendar.SECOND, 0);
     }
 
-    private final static Method deoptimizeMethod;
 
-    static {
-        Method m = null;
-        try {
-            m = XposedBridge.class.getDeclaredMethod("deoptimizeMethod", Member.class);
-        } catch (Throwable t) {
-            XposedBridge.log("E/" + TAG + " " + android.util.Log.getStackTraceString(t));
-        }
-        deoptimizeMethod = m;
-    }
-
-    static void deoptimizeMethod(Class<?> c, String n) throws InvocationTargetException, IllegalAccessException {
-        for (Method m : c.getDeclaredMethods()) {
-            if (deoptimizeMethod != null && m.getName().equals(n)) {
-                deoptimizeMethod.invoke(null, m);
-                if (BuildConfig.DEBUG)
-                    XposedBridge.log("D/" + TAG + " Deoptimized " + m.getName());
-            }
-        }
-    }
     /**
      * 调度定时执行
      *
@@ -222,14 +199,6 @@ public class ApplicationHook implements IXposedHookLoadPackage {
                 if (hooked) return;
                 appLloadPackageParam = loadPackageParam;
                 classLoader = appLloadPackageParam.classLoader;
-                // 在Hook Application.attach 之前，先 deoptimize LoadedApk.makeApplicationInner
-                try {
-                    Class<?> loadedApkClass = classLoader.loadClass("android.app.LoadedApk");
-                    deoptimizeMethod(loadedApkClass, "makeApplicationInner");
-                } catch (Throwable t) {
-                    Log.runtime(TAG, "deoptimize makeApplicationInner err:");
-                    Log.printStackTrace(TAG, t);
-                }
                 XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
